b5f19c705ed432caeea68c49e1694e1ceaaeb0b9
Jonas Nystrom <jonasnys@gmail.com>
2011-04-30 14:13:26 +0200

    first commit

--- views/phery/demo.php
+++ views/phery/demo.php
@@ -3,7 +3,6 @@
 <?php echo View::factory('phery/pheryinit') ?>
 
 <h1>Kophery demo</h1>
-
 <?php echo phery::link_to('A click on this link triggers Controller_PheryDemo::ph_testmethod function', 'testmethod', array('confirm' => 'Are you sure?', 'id' => 'special', 'url' => '', 'args' => array('hello' => 'This message is passed as data from the calling link!', 'more' => 'More phery data passed...'))); ?>
 <hr />
 <div id="target">This text in "target" div is going to be replaced!</div>
